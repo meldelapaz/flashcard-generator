07594f358c3077af50a35e62ff58a396da37b270
meldelapaz <meldelapaz.17@gmail.com>
2017-06-21 21:49:00 -0400

    got clozeCard.js to work

--- basicCard.js
+++ basicCard.js
@@ -12,96 +12,153 @@
 function flashCard(front,back) {
 	this.front = front;
 	this.back = back
-}
+};
 
 
 //FLASHCARDS ------------------------------------
-var one = new flashCard("In S1E1 Pilot: Who started their first day at Dunder Mifflin Scranton?", "Ryan Howard");
+var one = new flashCard("In S1E1 Pilot: Who started their first day at Dunder Mifflin Scranton?", "ryan howard");
 
-var two = new flashCard("In S1E2 Diversity Day: What famous comedian's stand up routine does Michael imitate?", "Chris Rock");
+var two = new flashCard("In S1E2 Diversity Day: What famous comedian's stand up routine does Michael imitate?", "chris rock");
 
-var three = new flashCard("In the episode Spooked, who do we find out is deathly afraid of mummies?", "Kevin Malone");
+var three = new flashCard("In the episode Spooked, who do we find out is deathly afraid of mummies?", "kevin malone");
 
-var four = new flashCard("Who plays Michael Scott?", "Steve Carell");
+var four = new flashCard("Who plays Michael Scott?", "steve carell");
 
-var five = new flashCard("Who does Michael hate more than anyone in the world?", "Toby Flenderson");
+var five = new flashCard("Who does Michael hate more than anyone in the world?", "toby flenderson");
 
 
 //INQUIRER PROMPTS -------------------------------
 var inquirer = require('inquirer');
+//var total = 0;
+
+// inquirer.prompt([/* Pass your questions in here */
+// 	{
+// 		type: 'input',
+// 		message: one.front,
+// 		name: 'questionOne'
+// 	}
+// ]).then(function (answers) {
+//     if (inquirer.one === one.back) {
+//     	console.log('Correct');
+//     	//total++;
+//     } else {
+//     	console.log('Wrong! Correct Answer: ' + one.back)
+//     }
+
+
+// 	inquirer.prompt([
+// 		{
+// 			type: 'input',
+// 			message: two.front,
+// 			name: 'two'
+// 		}
+// 	]).then(function (answers) {
+// 		if (inquirer.two === two.back) {
+// 	    	console.log('Correct')
+// 	    } else {
+// 	    	console.log('Wrong! Correct Answer: ' + two.back)
+// 	    }
+
+
+// 		inquirer.prompt([
+// 			{
+// 				type: 'input',
+// 				message: three.front,
+// 				name: 'three'
+// 			}
+// 		]).then(function (answers) {
+// 			if (inquirer.three === three.back) {
+// 		    	console.log('Correct')
+// 		    } else {
+// 		    	console.log('Wrong! Correct Answer: ' + three.back)
+// 		    }
+
+
+// 			inquirer.prompt([
+// 				{
+// 					type: 'input',
+// 					message: four.front,
+// 					name: 'four'
+// 				}
+// 			]).then(function (answers) {
+// 				if (inquirer.four === four.back) {
+// 			    	console.log('Correct')
+// 			    } else {
+// 			    	console.log('Wrong! Correct Answer: ' + four.back)
+// 			    }
+
+
+// 				inquirer.prompt([
+// 					{
+// 						type: 'input',
+// 						message: five.front,
+// 						name: 'five'
+// 					}
+// 				]).then(function (answers) {
+// 					if (inquirer.five === five.back) {
+// 				    	console.log('Correct')
+// 				    } else {
+// 				    	console.log('Wrong! Correct Answer: ' + five.back)
+// 				    }
+// 				});
+// 			});
+// 		});
+// 	});
+// });
 
 inquirer.prompt([/* Pass your questions in here */
 	{
 		type: 'input',
 		message: one.front,
-		name: 'one'
+		name: 'Uno'
+	},
+	{
+		type: 'input',
+		message: two.front,
+		name: 'Dos'
+	},
+	{
+		type: 'input',
+		message: three.front,
+		name: 'Tres'
+	},
+	{
+		type: 'input',
+		message: four.front,
+		name: 'Quatro'
+	},
+	{
+		type: 'input',
+		message: five.front,
+		name: 'Cinco'
 	}
-]).then(function (answers) {
-    if (inquirer.one === one.back) {
-    	console.log('Correct')
-    } else {
-    	console.log('Wrong! Correct Answer: ' + one.back)
-    }
 
-
-	inquirer.prompt([
-		{
-			type: 'input',
-			message: two.front,
-			name: 'two'
-		}
-	]).then(function (answers) {
-		if (inquirer.two === two.back) {
-	    	console.log('Correct')
-	    } else {
-	    	console.log('Wrong! Correct Answer: ' + two.back)
-	    }
-
-
-		inquirer.prompt([
-			{
-				type: 'input',
-				message: three.front,
-				name: 'three'
-			}
-		]).then(function (answers) {
-			if (inquirer.three === three.back) {
-		    	console.log('Correct')
-		    } else {
-		    	console.log('Wrong! Correct Answer: ' + three.back)
-		    }
-
-
-			inquirer.prompt([
-				{
-					type: 'input',
-					message: four.front,
-					name: 'four'
-				}
-			]).then(function (answers) {
-				if (inquirer.four === four.back) {
-			    	console.log('Correct')
-			    } else {
-			    	console.log('Wrong! Correct Answer: ' + four.back)
-			    }
-
-
-				inquirer.prompt([
-					{
-						type: 'input',
-						message: five.front,
-						name: 'five'
-					}
-				]).then(function (answers) {
-					if (inquirer.five === five.back) {
-				    	console.log('Correct')
-				    } else {
-				    	console.log('Wrong! Correct Answer: ' + five.back)
-				    }
-				});
-			});
-		});
-	});
+]).then(function (inquirer) {
+	if (inquirer.Uno === one.back) {
+		console.log('Correct')
+	} else {
+		console.log('Wrong! Correct Answer: ' + one.back)
+	}
+	if (inquirer.Dos === two.back) {
+		console.log('Correct')
+	} else {
+		console.log('Wrong! Correct Answer: ' + two.back)
+	}
+	if (inquirer.Tres === three.back) {
+		console.log('Correct')
+	} else {
+		console.log('Wrong! Correct Answer: ' + three.back)
+	}
+	if (inquirer.Quatro === four.back) {
+		console.log('Correct')
+	} else {
+		console.log('Wrong! Correct Answer: ' + four.back)
+	}
+	if (inquirer.Cinco === five.back) {
+		console.log('Correct')
+	} else {
+		console.log('Wrong! Correct Answer: ' + five.back)
+	}
 });
 
 //ANSWERS -------------------------------------------
--- clozeCard.js
+++ clozeCard.js
@@ -13,20 +13,81 @@
 
 //   * Use prototypes to attach these methods, wherever possible.
 
-
-function clozeCard(front,back) {
+//CONSTRUCTOR TO MAKE NEW CARDS ----------------
+function clozeCard(clozeDeleted, clozePartial, clozeFull) {
 	this.clozeDeleted = clozeDeleted;
-	this.patialText = patialText;
-	this.fullText = fullText;
-}
+	this.clozePartial = clozePartial;
+	this.clozeFull = clozeFull;
+};
 
 
+//FLASHCARDS ------------------------------------
+var one = new clozeCard("ryan howard", "In S1E1 Pilot: ... started their first day at Dunder Mifflin Scranton?", "In S1E1 Pilot: Ryan Howard started their first day at Dunder Mifflin Scranton?");
+
+var two = new clozeCard("chris rock", "In S1E2 Diversity Day: Michael immitates ...'s famous comedy stand up routine.", "In S1E2 Diversity Day: Michael immitates Chris Rock's famous comedy stand up routine.");
+
+var three = new clozeCard("kevin malone", "In the episode Spooked, we find out that ... is deathly afraid of mummies.", "In the episode Spooked, we find out that Kevin Malone is deathly afraid of mummies.");
+
+// var four = new flashCard("Who plays Michael Scott?", "steve carell");
+
+// var five = new flashCard("Who does Michael hate more than anyone in the world?", "toby flenderson");
 
 
+//INQUIRER PROMPTS -------------------------------
+var inquirer = require('inquirer');
 
 
-module.exports = clozeCard
+inquirer.prompt([/* Pass your questions in here */
+	{
+		type: 'input',
+		message: one.clozePartial,
+		name: 'Uno'
+	},
+	{
+		type: 'input',
+	 	message: two.clozePartial,
+		name: 'Dos'
+	},
+	{
+		type: 'input',
+		message: three.clozePartial,
+		name: 'Tres'
+	}
+	// {
+	// 	type: 'input',
+	// 	message: four.clozePartial,
+	// 	name: 'Quatro'
+	// }
+	// {
+	// 	type: 'input',
+	// 	message: five.front,
+	// 	name: 'Cinco'
+	// }
 
-
-
-
+]).then(function (inquirer) {
+	if (inquirer.Uno === one.clozeDeleted) {
+		console.log('Correct')
+	} else {
+		console.log('Wrong! Correct Answer: ' + one.clozeDeleted)
+	}
+	if (inquirer.Dos === two.clozeDeleted) {
+		console.log('Correct')
+	} else {
+		console.log('Wrong! Correct Answer: ' + two.clozeDeleted)
+	}
+	if (inquirer.Tres === three.clozeDeleted) {
+		console.log('Correct')
+	} else {
+		console.log('Wrong! Correct Answer: ' + three.clozeDeleted)
+	}
+	// if (inquirer.Quatro === four.clozeDeleted) {
+	// 	console.log('Correct')
+	// } else {
+	// 	console.log('Wrong! Correct Answer: ' + four.clozeDeleted)
+	// }
+	// if (inquirer.Cinco === five.back) {
+	// 	console.log('Correct')
+	// } else {
+	// 	console.log('Wrong! Correct Answer: ' + five.back)
+	// }
+});

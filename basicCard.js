0a288a59fb551a4b9762539b4c345bf072d7c515
meldelapaz <meldelapaz.17@gmail.com>
2017-06-26 08:15:55 -0400

    cleaned up code

--- basicCard.js
+++ basicCard.js
@@ -1,107 +1,22 @@
-// * This file should define a Node module that exports a constructor for creating basic flashcards, e.g.:
-//     `module.exports = BasicCard;`
-
-//   * The constructor should accept two arguments: `front` and `back`.
-
-//   * The constructed object should have a `front` property that contains the text on the front of the card.
-
-//   * The constructed object should have a `back` property that contains the text on the back of the card.
-
 //CONNECTING THE RUNAPP.JS WHERE THE CONSTRUCTORS ARE
 var runapp = require('./runapp.js');
 
 //INQUIRER NPM PACKAGE
 var inquirer = require('inquirer');
 
-
 //CREATING THE FLASHCARDS ------------------------------------
-var one = new runapp.flashCard("In S1E1 Pilot: Who started their first day at Dunder Mifflin Scranton?", "ryan howard");
+var one   = new runapp.flashCard("In S1E1 Pilot: Who started their first day at Dunder Mifflin Scranton?", "ryan howard");
 
-var two = new runapp.flashCard("In S1E2 Diversity Day: What famous comedian's stand up routine does Michael imitate?", "chris rock");
+var two   = new runapp.flashCard("In S1E2 Diversity Day: What famous comedian's stand up routine does Michael imitate?", "chris rock");
 
 var three = new runapp.flashCard("In the episode Spooked, who do we find out is deathly afraid of mummies?", "kevin malone");
 
-var four = new runapp.flashCard("Who plays Michael Scott?", "steve carell");
+var four  = new runapp.flashCard("Who plays Michael Scott?", "steve carell");
 
-var five = new runapp.flashCard("Who does Michael hate more than anyone in the world?", "toby flenderson");
-
-
-//This was my attempt in trying to get the result to show right after answering each question.
-	// inquirer.prompt([/* Pass your questions in here */
-	// 	{
-	// 		type: 'input',
-	// 		message: one.front,
-	// 		name: 'questionOne'
-	// 	}
-	// ]).then(function (answers) {
-	//     if (inquirer.one === one.back) {
-	//     	console.log('Correct');
-	//     	//total++;
-	//     } else {
-	//     	console.log('Wrong! Correct Answer: ' + one.back)
-	//     }
-
-	// 	inquirer.prompt([
-	// 		{
-	// 			type: 'input',
-	// 			message: two.front,
-	// 			name: 'two'
-	// 		}
-	// 	]).then(function (answers) {
-	// 		if (inquirer.two === two.back) {
-	// 	    	console.log('Correct')
-	// 	    } else {
-	// 	    	console.log('Wrong! Correct Answer: ' + two.back)
-	// 	    }
-
-	// 		inquirer.prompt([
-	// 			{
-	// 				type: 'input',
-	// 				message: three.front,
-	// 				name: 'three'
-	// 			}
-	// 		]).then(function (answers) {
-	// 			if (inquirer.three === three.back) {
-	// 		    	console.log('Correct')
-	// 		    } else {
-	// 		    	console.log('Wrong! Correct Answer: ' + three.back)
-	// 		    }
-
-	// 			inquirer.prompt([
-	// 				{
-	// 					type: 'input',
-	// 					message: four.front,
-	// 					name: 'four'
-	// 				}
-	// 			]).then(function (answers) {
-	// 				if (inquirer.four === four.back) {
-	// 			    	console.log('Correct')
-	// 			    } else {
-	// 			    	console.log('Wrong! Correct Answer: ' + four.back)
-	// 			    }
-
-	// 				inquirer.prompt([
-	// 					{
-	// 						type: 'input',
-	// 						message: five.front,
-	// 						name: 'five'
-	// 					}
-	// 				]).then(function (answers) {
-	// 					if (inquirer.five === five.back) {
-	// 				    	console.log('Correct')
-	// 				    } else {
-	// 				    	console.log('Wrong! Correct Answer: ' + five.back)
-	// 				    }
-	// 				});
-	// 			});
-	// 		});
-	// 	});
-	// });
-
-
+var five  = new runapp.flashCard("Who does Michael hate more than anyone in the world?", "toby flenderson");
 
 //INQUIRER PROMPTS -------------------------------
-inquirer.prompt([/* Pass your questions in here */
+inquirer.prompt([
 	{
 		type: 'input',
 		message: one.front,
@@ -130,30 +45,55 @@ inquirer.prompt([/* Pass your questions in here */
 
 //RESULTS/ANSWERS ----------------------------------------
 ]).then(function (inquirer) {
+	
 	if (inquirer.Uno === one.back) {
+	
 		console.log('Correct')
+	
 	} else {
+	
 		console.log('Wrong! Correct Answer: ' + one.back)
+	
 	}
+	
 	if (inquirer.Dos === two.back) {
+	
 		console.log('Correct')
+	
 	} else {
+	
 		console.log('Wrong! Correct Answer: ' + two.back)
+	
 	}
+	
 	if (inquirer.Tres === three.back) {
+	
 		console.log('Correct')
+	
 	} else {
+	
 		console.log('Wrong! Correct Answer: ' + three.back)
+	
 	}
+	
 	if (inquirer.Quatro === four.back) {
+	
 		console.log('Correct')
+	
 	} else {
+	
 		console.log('Wrong! Correct Answer: ' + four.back)
+	
 	}
+	
 	if (inquirer.Cinco === five.back) {
+	
 		console.log('Correct')
+	
 	} else {
+	
 		console.log('Wrong! Correct Answer: ' + five.back)
+	
 	}
-});
 
+});
--- clozeCard.js
+++ clozeCard.js
@@ -1,34 +1,15 @@
-// * This file should define a Node module that exports a constructor for creating cloze-deletion flashcards, e.g.:
-//     `module.exports = ClozeCard;`
-
-//   * The constructor should accept two arguments: `text` and `cloze`.
-
-//   * The constructed object should have a `cloze` property that contains _only_ the cloze-deleted portion of the text.
-
-//   * The constructed object should have a `partial` property that contains _only_ the partial text.
-
-//   * The constructed object should have a `fullText` property that contains _only_ the full text.
-
-//   * The constructor should throw or log an error when the cloze deletion does _not_ appear in the input text.
-
-//   * Use prototypes to attach these methods, wherever possible.
-
-//CONNECTING RUNAPP.JS WHERE THE CONSTRUCTORS ARE ----------------
+// CONNECTING RUNAPP.JS WHERE THE CONSTRUCTORS ARE ----------------
 var runapp = require('./runapp.js');
 
+// CREATING NEW FLASHCARDS ------------------------------------
+var one   = new runapp.clozeCard("ryan howard", "In S1E1 Pilot: ... started their first day at Dunder Mifflin Scranton?", "In S1E1 Pilot: Ryan Howard started their first day at Dunder Mifflin Scranton?");
 
-//CREATING NEW FLASHCARDS ------------------------------------
-var one = new runapp.clozeCard("ryan howard", "In S1E1 Pilot: ... started their first day at Dunder Mifflin Scranton?", "In S1E1 Pilot: Ryan Howard started their first day at Dunder Mifflin Scranton?");
-
-var two = new runapp.clozeCard("chris rock", "In S1E2 Diversity Day: Michael immitates ...'s famous comedy stand up routine.", "In S1E2 Diversity Day: Michael immitates Chris Rock's famous comedy stand up routine.");
+var two   = new runapp.clozeCard("chris rock", "In S1E2 Diversity Day: Michael immitates ...'s famous comedy stand up routine.", "In S1E2 Diversity Day: Michael immitates Chris Rock's famous comedy stand up routine.");
 
 var three = new runapp.clozeCard("kevin malone", "In the episode Spooked, we find out that ... is deathly afraid of mummies.", "In the episode Spooked, we find out that Kevin Malone is deathly afraid of mummies.");
 
-
-
-//INQUIRER PROMPTS -------------------------------
+// INQUIRER PROMPTS -------------------------------
 var inquirer = require('inquirer');
-
 
 inquirer.prompt([/* Pass your questions in here */
 	{
@@ -49,20 +30,35 @@ inquirer.prompt([/* Pass your questions in here */
 
 //RESULTS/ANSWERS----------------------------
 ]).then(function (inquirer) {
+
 	if (inquirer.Uno === one.clozeDeleted) {
-		console.log('Correct')
+
+		console.log('Correct');
+
 	} else {
-		console.log('Wrong! Correct Answer: ' + one.clozeDeleted)
+
+		console.log('Wrong! Correct Answer: ' + one.clozeDeleted);
+
 	}
+
 	if (inquirer.Dos === two.clozeDeleted) {
-		console.log('Correct')
+
+		console.log('Correct');
+
 	} else {
-		console.log('Wrong! Correct Answer: ' + two.clozeDeleted)
+
+		console.log('Wrong! Correct Answer: ' + two.clozeDeleted);
+
 	}
+
 	if (inquirer.Tres === three.clozeDeleted) {
-		console.log('Correct')
+
+		console.log('Correct');
+
 	} else {
-		console.log('Wrong! Correct Answer: ' + three.clozeDeleted)
+
+		console.log('Wrong! Correct Answer: ' + three.clozeDeleted);
+
 	}
 	
 });
